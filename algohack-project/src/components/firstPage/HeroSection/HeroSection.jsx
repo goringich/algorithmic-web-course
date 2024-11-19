2ed587dce7918ea3eb4fd30a3865d5ab1a49a62c
goringich <actingsv@gmail.com>
2024-11-19 12:49:49 +0300

    animation

--- algohack-project/src/components/firstPage/HeroSection/HeroSection.jsx
+++ algohack-project/src/components/firstPage/HeroSection/HeroSection.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './HeroSection.module.scss';
 import person from '../../../assets/images/MainPage/anime-person/Character&device.png';
 import table from '../../../assets/images/MainPage/anime-person/Group 3490.png';
 
+const Levitate = () => {
+  const [isHovering, setIsHovering] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+    setTimeout(() => setIsAnimating(true), 500);
+  };
+
+  const handleMouseLeave = () => {
+    setTimeout(() => setIsAnimating(false), 500);
+    setIsHovering(false);
+  };
+
+  return (
+    <img
+      className={`
+        ${styles.image__person} 
+        ${styles.levitate} 
+        ${isHovering ? styles.hover : ''} 
+        ${isAnimating ? styles.animating : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      src={person}
+      alt="Person sitting with a laptop"
+    />
+  );
+};
 const HeroSection = () => {
   return (
     <section className={styles.hero}>
@@ -19,8 +47,8 @@ const HeroSection = () => {
         <button className={styles.button}>Начать →</button>
       </div>
       <div className={styles.image}>
-        <img className={styles.image__person} src={person} alt="Person sitting with a laptop" />
-        <img  className={styles.image__table} src={table} alt="Person sitting with a laptop" />
+        <Levitate />
+        <img className={styles.image__table} src={table} alt="Table illustration" />
       </div>
     </section>
   );
