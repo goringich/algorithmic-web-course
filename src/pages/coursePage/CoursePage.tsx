296abb2cd1531edcf7ee87165356a202df7b52ab
Julia <uliachez@gmail.com>
2025-01-18 21:29:32 +0300

    normalization of framed text

--- src/pages/coursePage/CoursePage.tsx
+++ src/pages/coursePage/CoursePage.tsx
@@ -9,6 +9,16 @@ const CourseContent: React.FC = () => {
     setOpenSection(openSection === ind ? null : ind);
   };
 
+  const isUnderlined = (text: string) => {
+    const underlinedItems = [
+      "Дерево отрезков (ДО)",
+      "Дерево Фенwick",
+      "Процесс дерева отрезков",
+      "Ленивое дерево отрезков"
+    ];
+    return underlinedItems.includes(text.trim());
+  };
+
   return (
     <div className={styles.course__content}>
       <h1 className={styles.title}>Содержание курса</h1>
@@ -19,19 +29,21 @@ const CourseContent: React.FC = () => {
               className={styles.section__title} 
               onClick={() => toggleSection(index)}
             >
-              {/* Кружок с номером */}
               <div className={styles.circle}>
                 <span className={styles.number}>{index + 1}</span>
               </div>
-              {/* Заголовок секции */}
               <h2 className={styles.subtitle}>{section.title}</h2>
-              {/* Стрелка */}
               <span className={`${styles.arrow} ${openSection === index ? styles.open : ''}`}></span>
             </div>
             {openSection === index && (
               <ul className={styles.course__box}>
                 {section.subSections.map((sub, idx) => (
-                  <li key={idx} className={styles.listItem}>{sub}</li>
+                  <li
+                    key={idx}
+                    className={`${styles.listItem} ${isUnderlined(sub) ? styles.underlined : ""} ${styles._listItem_1hgjm_54}`}
+                  >
+                    {sub}
+                  </li>
                 ))}
               </ul>
             )}
