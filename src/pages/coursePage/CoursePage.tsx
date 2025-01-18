8e251d46de9625452ea0d048f936dd54cc6e4716
Julia <uliachez@gmail.com>
2025-01-18 16:39:39 +0300

    Page design without a card in the background

--- src/pages/coursePage/CoursePage.tsx
+++ src/pages/coursePage/CoursePage.tsx
@@ -1,41 +1,45 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import styles from './CoursePage.module.scss';
 import courseContent from "../../assets/dataBase/TitlesData.json";
 
-interface CourseSection {
-  title: string;
-  subSections: string[];
-}
-  
 const CourseContent: React.FC = () => {
   const [openSection, setOpenSection] = useState<number | null>(null);
 
   const toggleSection = (ind: number) => {
     setOpenSection(openSection === ind ? null : ind);
-  }
+  };
 
   return (
     <div className={styles.course__content}>
-      <h1>Содержание курса</h1>
+      <h1 className={styles.title}>Содержание курса</h1>
       <div>
         {courseContent.map((section, index) => (
-          <div key={index}>
-            <div className={styles.section__title} onClick = {() => toggleSection(index)}>
-              <h2>{section.title}</h2>
-              <span>{openSection === index ? '▲' : '▼'}</span>
+          <div key={index} className={styles.section}>
+            <div 
+              className={styles.section__title} 
+              onClick={() => toggleSection(index)}
+            >
+              {/* Кружок с номером */}
+              <div className={styles.circle}>
+                <span className={styles.number}>{index + 1}</span>
+              </div>
+              {/* Заголовок секции */}
+              <h2 className={styles.subtitle}>{section.title}</h2>
+              {/* Стрелка */}
+              <span className={`${styles.arrow} ${openSection === index ? styles.open : ''}`}></span>
             </div>
             {openSection === index && (
-              <ul className={`${styles.subsections} ${openSection === index ? styles.open : ''}`}>
+              <ul className={styles.course__box}>
                 {section.subSections.map((sub, idx) => (
-                  <li key={idx}>{sub}</li>
+                  <li key={idx} className={styles.listItem}>{sub}</li>
                 ))}
               </ul>
             )}
-          </div> 
+          </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CourseContent;
