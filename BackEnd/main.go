72498dc6069734e24bc80d2fb57cce3d63f59264
Igor <actingsv@gmail.com>
2025-01-22 13:09:38 +0300

    continue backend development

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -3,20 +3,18 @@ package main
 import (
   "log"
 
-  "your_project_path/back/content_loader" // Обновите на ваш путь
-  "your_project_path/back/database"      // Обновите на ваш путь
+  "BackEnd/content_loader"
+  "BackEnd/database"
 )
 
 func main() {
-  // Подключение к базе данных
   db, err := database.Connect()
   if err != nil {
     log.Fatalf("Не удалось подключиться к базе данных: %v", err)
   }
   defer db.Close()
 
-  // Загрузка контента из папки DataBase/content
-  contentPath := "DataBase/content" // Убедитесь, что путь указан правильно
+  contentPath := "DataBase/content"
   err = content_loader.LoadContent(db, contentPath)
   if err != nil {
     log.Fatalf("Ошибка при загрузке контента: %v", err)
