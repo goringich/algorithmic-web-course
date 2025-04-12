920063bd8002e498ff10c4f75fbd4ba3da079a50
Anastasia <n_pishaeva@mail.ru>
2025-04-12 16:04:58 +0300

    fixed code display and added theory

--- FrontEnd/src/pages/algorithmsPage/components/CodeBlock.tsx
+++ FrontEnd/src/pages/algorithmsPage/components/CodeBlock.tsx
@@ -1,21 +1,37 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark  } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { styled } from '@mui/system';
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { styled } from "@mui/system";
 import { Theme } from "@mui/material/styles";
-import {Grid2} from "@mui/material";
+import { Grid2 } from "@mui/material";
 
 const Code = styled(Grid2)<{ theme: Theme }>(({ theme }) => ({
   width: "90%",
-  boxShadow: theme.shadows[3], 
+  boxShadow: theme.shadows[3],
   margin: "auto",
+  marginBottom: theme.spacing(4)
 }));
 
 const CodeBlock = ({ code, language = "cpp" }) => {
   return (
     <Code>
-        <SyntaxHighlighter language={language} style={oneDark}>
+      <SyntaxHighlighter
+        language={language}
+        style={oneDark}
+        wrapLongLines={true}
+        customStyle={{
+          whiteSpace: "pre-wrap",
+          wordBreak: "break-word",
+          overflowX: "hidden",
+        }}
+        codeTagProps={{
+          style: {
+            whiteSpace: "pre-wrap",
+            wordBreak: "break-word",
+          },
+        }}
+      >
         {code}
-        </SyntaxHighlighter>
+      </SyntaxHighlighter>
     </Code>
   );
 };
